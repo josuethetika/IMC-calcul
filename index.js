842c9de73243bde2009d045e26aadae4c1542f60
thetika <129292155+josuethetika@users.noreply.github.com>
2023-05-27 19:11:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ function calculimc(){
     let size = document.getElementById("size").value;
     let weight = document.getElementById("weight").value;  
     let advise = document.getElementById("advise");
-    document.getElementById("result").innerHTML = weight /(size*size);
-    if ( weight /(size*size)<18.5 ){
+    let imc=weight /(size*size);
+    document.getElementById("result").innerHTML = imc;
+    if ( imc<18.5 ){
         advise.textContent ='vous ête en inssufisance pondérale';
         advise.style.background = 'red';
         advise.style.color = 'white' ;
   }
-  else if ( weight /(size*size)<25 ){
+  else if ( imc<25 ){
     advise.textContent ='vous avez une corpulance normale';
     advise.style.background = 'red';
     advise.style.color = 'white';
 }
 
- else if ( weight /(size*size)<30 ){
+ else if ( imc<30 ){
         advise.textContent ='vous ête en surpoids';
         advise.style.background = 'red';
         advise.style.color = 'white' ;
   }
-   else if ( weight /(size*size)<35 ){
+   else if ( imc<35 ){
     advise.textContent ='vous ête en obesité moderer';
     advise.style.background = 'red';
     advise.style.color = 'white' ;
 }
-else if ( weight /(size*size)<40 ){
+else if ( imc<40 ){
     advise.textContent ='vous ête en obesité sevère';
     advise.style.background = 'red';
     advise.style.color = 'white' ;
 }
-else if ( weight /(size*size)>40 ){
+else if ( imc>40 ){
     advise.textContent ='vous ête en obesité morbide ou massive';
     advise.style.background = 'red';
     advise.style.color = 'white' ;
